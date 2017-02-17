87b84f2b898c6e9b467a753e7af5869cabb59203
Gary Bivings <fgb@blancobivings.com>
2017-02-17 16:56:54 -0500

    About page

--- routes.py
+++ routes.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
 
